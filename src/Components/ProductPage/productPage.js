c0178dd58cfb23a53af9724c0a42149fe79e4dd6
bibinprathap <bibinprathap@gmail.com>
2021-05-05 08:45:47 +0400

    modified product page

--- src/Components/ProductPage/productPage.js
+++ src/Components/ProductPage/productPage.js
@@ -59,12 +59,136 @@ const ProductSwiper = () => {
           }
         }
       }
-
+     activeSlideKey={(key) => console.log(key)}
+     onActiveIndexChange={(key) => console.log(key)}
     >
-      <SwiperSlide>   <img className="w-100" src={require(`../../Asset/images/promo1.png`).default}/></SwiperSlide>
-      <SwiperSlide>   <img className="w-100" src={require(`../../Asset/images/promo2.png`).default}/></SwiperSlide>
-      <SwiperSlide>   <img className="w-100" src={require(`../../Asset/images/promo3.png`).default}/></SwiperSlide>
-      <SwiperSlide>Slide 4</SwiperSlide>
+      <SwiperSlide key={1}>   <img className="w-100 swipe-ele" src={require(`../../Asset/images/promo1.png`).default}/></SwiperSlide>
+      <SwiperSlide key={2}>   <img className="w-100 swipe-ele" src={require(`../../Asset/images/promo2.png`).default}/></SwiperSlide>
+      <SwiperSlide key={3}>   <img className="w-100 swipe-ele" src={require(`../../Asset/images/promo3.png`).default}/></SwiperSlide>
+      <SwiperSlide key={4}>Slide 4</SwiperSlide>
+    </Swiper>
+  );
+};
+
+const RelatedProductSwiper = () => {
+  
+  return (
+    <Swiper
+      spaceBetween={40}
+      slidesPerView={6}
+      loop={true}
+      pagination={{ clickable: true, renderBullet: (index, className) => `<span class="dot swiper-pagination-bullet">${index}</span>` }}
+      breakpoints={
+        {
+          1920: {
+            slidesPerView: 8,
+            spaceBetween: 50,
+          },
+          1024: {
+            slidesPerView: 5,
+            spaceBetween: 50,
+          },
+          992: {
+            slidesPerView: 4,
+            spaceBetween: 20
+          },
+          768: {
+            slidesPerView: 3,
+            spaceBetween: 40,
+          },
+    
+          640: {
+            slidesPerView: 1,
+            spaceBetween: 20
+          },
+    
+          320: {
+            slidesPerView: 1,
+            spaceBetween: 20
+          }
+        }
+      }
+     activeSlideKey={(key) => console.log(key)}
+     onActiveIndexChange={(key) => console.log(key)}
+    >
+      {Array(8).fill(null).map((index) => (
+        <SwiperSlide key={index}><div className="product px-4 pb-3">
+        <div className="leftbadge">ONLY 3 LEFT</div>
+        <div className="py-3">
+          <p>Frido Ultra Comfortable Copper</p>
+          <h5>$1.659,00</h5>
+          <span class="badge rounded-pill bg-info text-dark px-2">Info</span>
+        </div>
+        <div className="w-100 px-3">
+        <button className="btn btn-sm btn-warning btn-block">
+          Add to cart
+        </button>
+        </div>
+ 
+        <div>
+     
+        </div>
+        </div></SwiperSlide>
+      ))}
+
+    </Swiper>
+  );
+};
+
+const Bestsellers = () => {
+  return (
+    <Swiper
+      spaceBetween={30}
+      slidesPerView={3}
+      breakpoints={
+        {
+          768: {
+            slidesPerView: 6,
+            spaceBetween: 40,
+          },
+          1024: {
+            slidesPerView: 8,
+            spaceBetween: 50,
+          },
+          640: {
+            slidesPerView: 3,
+            spaceBetween: 20
+          },
+    
+          320: {
+            slidesPerView: 1,
+            spaceBetween: 0
+          }
+        }
+      }
+     activeSlideKey={(key) => console.log(key)}
+     onActiveIndexChange={(key) => console.log(key)}
+    >
+       {Array(8).fill(null).map((index) => (
+        <SwiperSlide key={index}><div className="product pb-3">
+        <div className="w-100" style={{height: "50%", backgroundColor: "#E7E9EB", borderRadius: 20}}></div>
+        <div className="promobadge">
+        <div className="leftbadge">New</div>
+        <div className="leftbadge2">Hot</div>
+        </div>
+        
+        <div className="w-100 px-4">
+        <div className="py-3">
+          <p>Frido Ultra Comfortable Copper</p>
+          <h6>$1.659,00</h6>
+          <span class="badge rounded-pill bg-info text-dark px-3">Info</span>
+        </div>
+        <div className="w-100 px-2">
+        <button className="btn btn-sm btn-warning btn-block">
+          Add to cart
+        </button>
+        </div>
+ 
+        </div>
+       
+      
+        </div></SwiperSlide>
+      ))}
     </Swiper>
   );
 };
@@ -531,17 +655,19 @@ class ProductPage extends React.Component {
            
 
              </div>
-             <div className="col-12" style={{overflow: "hidden"}}>
-             <BlockSpace layout="divider-nl" />
-            <BlockProductsCarousel
-                blockTitle={'Bestsellers'}
-                layout="grid-5"
-                loading={featuredProducts.isLoading}
-                products={featuredProducts.data}
-                groups={featuredProducts.tabs}
-                currentGroup={featuredProducts.tabs.find((x) => x.current)}
-                onChangeGroup={featuredProducts.handleTabChange}
-            />
+             <div className="col-12 py-3 pt-5">
+               <h4>Related Products</h4>
+             </div>
+             <div className="col-12 related-product" style={{overflow: "hidden"}}>
+             <RelatedProductSwiper />
+             </div>
+             <div className="col-12 py-3 pt-5">
+               <h4>Bestsellers</h4>
+             </div>
+             <div className="col-12 bestseller">
+               <Bestsellers />
+             </div>
+             {/* <div className="col-12" style={{overflow: "hidden"}}>
              <BlockSpace layout="divider-nl" />
             <BlockProductsCarousel
                 blockTitle={'Most Purchased'}
@@ -552,7 +678,7 @@ class ProductPage extends React.Component {
                 currentGroup={featuredProducts.tabs.find((x) => x.current)}
                 onChangeGroup={featuredProducts.handleTabChange}
             />
-             </div>
+             </div> */}
           </div>
        
           
