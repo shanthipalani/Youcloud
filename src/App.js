312954acde6cc74e9bc2a6ae191b0b9b30c979af
Sarvath30 <sarvathcr7@gmail.com>
2021-05-19 22:15:20 +0530

    added components and routing

--- src/App.js
+++ src/App.js
@@ -28,13 +28,13 @@ import Password from './Components/Password_recovery/pwd'
 import ProductDetailPage from './Components/ProductDetailPage/productDetailPage'
 import FilterPage from './Components/Filter/filterPage'
 import { Switch, Route} from 'react-router-dom'
-import Nav from './Components/navigation/nav'
+// import Nav from './Components/navigation/nav'
 import Cart from './Components/Cart/cart'
 import Checkout from './Components/Cart/checkout'
 import Confirmation from './Components/Cart/confirmation'
 import Shiporder from './Components/Shipment_order/shipmentorder';
 import Userprofile from './Components/Userprofile/userprofile'
-
+import Invoice from './Components/Invoice/invoice'
 
 
 function App() {
@@ -47,7 +47,8 @@ function App() {
           <Route path="/forgotpassword" exact component={Password} />
           <Route path="/SignUp" exact component={Signup}/>
           <Route path="/SignIn" exact component={Form}/>
-          {/* <Route path=/> */}
+          <Route path="/Userprofile" exact component={Userprofile}/>
+          <Route path="/Cart" exact component={Cart}/>
         </Switch>
           {/* <Form/> */}
           <Form2/>
@@ -63,6 +64,7 @@ function App() {
           <Confirmation />
           <Shiporder />
           <Userprofile />
+          <Invoice />
           
         
     </div>
